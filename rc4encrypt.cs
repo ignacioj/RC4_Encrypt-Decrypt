4f14bd4f06cb76d105e361248e8218e940823e91
nach <nach@local.com>
2015-10-05 23:26:56 +0200

    License information.

--- rc4encrypt.cs
+++ rc4encrypt.cs
@@ -1,19 +1,4 @@
-﻿/*
-Copyright 2015 Ignacio J. Perez J.
-
-Licensed under the Apache License, Version 2.0 (the ""License"");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an ""AS IS"" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -42,16 +27,31 @@ namespace RC4ED
             }
             if (!comprueba)
             {
-                Console.WriteLine(@"Ignacio J. Perez J.
+                Console.WriteLine(@"
 RC4 encryption/decryption
-Usage:
-archivo_datos: binary file to decode
-archivo_clave: binary file with the key
+
+Copyright 2015 Ignacio J. Perez J.
+
+Licensed under the Apache License, Version 2.0 (the ""License"");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an ""AS IS"" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+- archivo_datos: binary file to decode
+- archivo_clave: binary file with the key
 
 The output will be stored in a binary file named archivo_datos.out.dat.
 It will show the printable characters on screen.
+Usage:
 
-rc4ed archivo_datos archivo_clave 
+>rc4ed.exe archivo_datos archivo_clave 
 
 ");
             }
